8e042f324ec5148a8170e8d6f65e856f3bbceacd
your_username <phanvothanhtai1007@gmail.com>
2025-02-08 13:32:26 +0700

    init shorts

--- src/routers/router.jsx
+++ src/routers/router.jsx
@@ -11,6 +11,7 @@ const router = createBrowserRouter([
     element: <RootLayout />,
     children: [
       { path: "", element: <Home /> },
+      { path: "shorts", element: <Shorts /> },
     //   { path: "search", element: <Search /> },
     //   { path: "watch", element: <Watch /> },
     ],
