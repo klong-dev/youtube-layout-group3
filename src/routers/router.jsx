28c09414689144ac5fd429801e97c5629c111eba
Hoàng Kim Long <longhoang8204@gmail.com>
2025-02-09 08:25:40 +0700

    chore: added env example

--- src/routers/router.jsx
+++ src/routers/router.jsx
@@ -1,8 +1,6 @@
 import { createBrowserRouter } from "react-router";
 import { Home } from "../pages/Home/Home";
 import RootLayout from "../layouts/RootLayout";
-// import { Search } from "@/pages/Search/Search";
-// import { Watch } from "@/pages/Watch/Watch";
 import { NotFound } from "../pages/NotFound/NotFound";
 
 const router = createBrowserRouter([
@@ -11,8 +9,6 @@ const router = createBrowserRouter([
     element: <RootLayout />,
     children: [
       { path: "", element: <Home /> },
-    //   { path: "search", element: <Search /> },
-    //   { path: "watch", element: <Watch /> },
     ],
     errorElement: <NotFound />,
   },
